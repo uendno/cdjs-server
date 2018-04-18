e7039a506bdaece52061422b2bc8fd71bceb967a
Tran Viet Thang <uendno@gmail.com>
2018-04-18 17:40:57 +0700

    Add 'admin' role for initial user

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -2,24 +2,25 @@ const randomstring = require('randomstring');
 const User = require('../models/User');
 
 exports.prepareAdminUser = () => {
-  // Prepare admin user
-  return User.findOne({
-    email: 'admin@cdjs.com'
-  })
-    .then(user => {
-      if (!user) {
-        // const password = randomstring.generate();
-          const password = 12345678;
+    // Prepare admin user
+    return User.findOne({
+        email: 'admin@cdjs.com'
+    })
+        .then(user => {
+            if (!user) {
+                // const password = randomstring.generate();
+                const password = 12345678;
 
-        console.log("Initial username: admin@cdjs.com");
-        console.log("Initial password: " + password);
+                console.log("Initial username: admin@cdjs.com");
+                console.log("Initial password: " + password);
 
-        const admin = new User({
-          email: 'admin@cdjs.com',
-          password
+                const admin = new User({
+                    email: 'admin@cdjs.com',
+                    password,
+                    role: 'admin'
+                });
+
+                return admin.save();
+            }
         });
-
-        return admin.save();
-      }
-    });
 };
